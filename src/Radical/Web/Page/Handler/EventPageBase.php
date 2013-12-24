c88a3a95cfed326ae88756ca00f100ea9704ee21
splitice <mat999@gmail.com>
2013-12-24 21:11:24 +1100

    code commit

---
+++ src/Radical/Web/Page/Handler/EventPageBase.php
@@ -0,0 +1,6 @@
+<?php
+namespace Radical\Web\Page\Handler;
+
+abstract class EventPageBase extends HTMLPageBase {
+	use TEventPageBase;
+}
---
+++ src/Radical/Web/Page/Handler/TEventPageBase.php
@@ -0,0 +1,75 @@
+<?php
+namespace Radical\Web\Page\Handler;
+
+use Radical\Web\Form\Security\KeyStorage;
+use Radical\Web\Form\Security\Key;
+use Radical\Web\Page\Controller\Special\Redirect;
+use Radical\Utility\Net\URL;
+
+trait TEventPageBase {
+	protected $eventKey;
+	
+	protected function _processEvent($post = true){
+		$id = Key::fromRequest($post);
+		if(!empty($id)){
+			$key = KeyStorage::GetKey($id);
+			if($key){
+				$this->eventKey = $key;
+				$result = $key->Callback();
+				if($result){
+					return $result;
+				}
+			}else{
+				throw new \Exception('Event invalid (session timeout?)');
+			}
+		}
+	}
+	
+	/**
+	 * Intercept Execute calls and check for POST events
+	 * If there is a post event submission do it.
+	 *
+	 * @see Web\Page\Handler.PageBase::Execute()
+	 */
+	function execute($method = 'GET'){
+		//Check for an event
+		$t = $this;
+		$processed = false;
+		$event_func = function() use($t, $method, &$processed) { 
+			$r = $t->_processEvent($method == 'POST');
+			if($r) {
+				$processed = true;
+				$request = new PageRequest($r);
+				return $request->execute($method);
+			}
+		};
+		
+		if(method_exists($this, 'event_execute')){
+			$r = $this->event_execute($event_func);
+		}else{			
+			$r = $event_func();
+		}
+		
+		if($processed)
+			return $r;
+
+		//Normal execution
+		return parent::Execute($method);
+	}
+	
+	protected function event_redirect(){
+		if($_SERVER['REQUEST_METHOD'] == 'POST')
+			return new Redirect((string)URL::fromRequest());
+		
+		$url = URL::fromRequest();
+		$qs = $url->getQuery();
+		foreach($qs as $k=>$v){
+			if(substr($k, 0, 2) == '__'){
+				unset($qs[$k]);
+			}
+		}
+		$url->setQuery($qs);
+
+		return new Redirect((string)$url);
+	}
+}
