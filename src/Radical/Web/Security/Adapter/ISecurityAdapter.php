b0b38559c61c5ee8b2922a819f7be86eb488b640
splitice <mat999@gmail.com>
2017-02-21 18:46:11 +1100

    phpdoc

--- src/Radical/Web/Security/Adapter/ISecurityAdapter.php
+++ src/Radical/Web/Security/Adapter/ISecurityAdapter.php
@@ -6,7 +6,17 @@ use Radical\Web\Security\Keys\Key;
 
 interface ISecurityAdapter
 {
+	/**
+	 * @param $key
+	 * @return Key
+	 */
 	function get($key);
 	function add(Key $key);
+
+	/**
+	 * @param callable $call
+	 * @param int $ttl
+	 * @return Key
+	 */
 	function newKey($call, $ttl = -1);
 }
